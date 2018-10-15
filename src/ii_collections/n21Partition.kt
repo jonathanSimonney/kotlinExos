a8377739d1feb6845ad87e0dd818fd30251ec6f3
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-10-15 17:55:43 +0200

    ii exercise done.

--- src/ii_collections/n21Partition.kt
+++ src/ii_collections/n21Partition.kt
@@ -12,5 +12,9 @@ fun example8() {
 
 fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
     // Return customers who have more undelivered orders than delivered
-    todoCollectionTask()
+    //todoCollectionTask()
+    return this.customers.asSequence().filter { customer ->
+        val (delivered, undelivered) = customer.orders.partition { it.isDelivered }
+         undelivered.size > delivered.size
+    }.toSet()
 }
